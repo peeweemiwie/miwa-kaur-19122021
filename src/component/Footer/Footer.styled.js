51f42a2ce0f8a8762a1e011eb38ccb99fe46a726
peeweemiwie <miwayagi@gmail.com>
2021-12-27 16:25:11 -0500

    Add react-icons. Update footer and header.

--- src/component/Footer/Footer.styled.js
+++ src/component/Footer/Footer.styled.js
@@ -1,9 +1,23 @@
 import styled from 'styled-components';
 import { Color, FontSize } from '../variables';
-const { lightGray } = Color;
-const { fontReg } = FontSize;
+const { lightGray, cardBg } = Color;
+const { fontReg, fontMd } = FontSize;
+
 export const StyledFooter = styled.footer`
+	background-color: ${({ bgColor }) => bgColor || cardBg};
 	color: ${lightGray};
 	font-size: ${fontReg};
 	margin-top: auto;
+	padding: 3rem 0;
+	small {
+		display: block;
+		text-align: center;
+		margin-top: 2rem;
+	}
+`;
+
+export const MediaLinks = styled.div`
+	a {
+		font-size: ${fontMd};
+	}
 `;
