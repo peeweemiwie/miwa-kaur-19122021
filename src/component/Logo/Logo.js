f3da3dae1c40d5d3b6eee550d4f1ca4bd28812ab
peeweemiwie <miwayagi@gmail.com>
2021-12-30 16:25:34 -0500

    Remove Logo component

--- src/component/Logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import { StyledLogo } from './Logo.styled';
-
-export const Logo = () => {
-	return (
-		<StyledLogo>
-			<img src='./images/logo.svg' alt='Logo: Miwa Yagi Kaur' />
-		</StyledLogo>
-	);
-};
